73b3b39477fe5eca499600f5046a244d246569fd
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-06-02 13:04:40 -0500

    :construction: Connect to the API

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../assets/styles/App.scss";
 
 import Header from "../components/Header";
@@ -8,18 +8,31 @@ import Carousel from "../components/Carousel";
 import CarouselItem from "../components/CarouselItem";
 import Footer from "../components/Footer";
 
-const App = () => (
-  <div className='App'>
-    <Header />
-    <Search />
-    <Categories title='Uno'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
-    <Footer />
-  </div>
-);
+const App = () => {
+  const [videos, setVideos] = useState([]);
+
+  // The second parameter is for not create and infinity loop listened changes
+  useEffect(() => {
+    fetch("http://localhost:3000/initialState")
+      .then((response) => response.json())
+      .then((data) => setVideos(data));
+  }, []);
+
+  console.log(videos);
+
+  return (
+    <div className='App'>
+      <Header />
+      <Search />
+      <Categories title='Uno'>
+        <Carousel>
+          <CarouselItem />
+          <CarouselItem />
+        </Carousel>
+      </Categories>
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
