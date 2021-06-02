a8d3e8d3aa23c9847d923c8b2b304239c918b614
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-06-02 13:32:01 -0500

    :construction: Information API

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -9,25 +9,33 @@ import CarouselItem from "../components/CarouselItem";
 import Footer from "../components/Footer";
 
 const App = () => {
-  const [videos, setVideos] = useState([]);
+  const [videos, setVideos] = useState({ mylist: [], trends: [], originals: [] });
+  const API = "http://localhost:3000/initialState";
 
   // The second parameter is for not create and infinity loop listened changes
   useEffect(() => {
-    fetch("http://localhost:3000/initialState")
+    fetch(API)
       .then((response) => response.json())
-      .then((data) => setVideos(data));
+      .then((data) => setVideos(data))
+      .catch((error) => console.log(error));
   }, []);
-
-  console.log(videos);
 
   return (
     <div className='App'>
       <Header />
       <Search />
-      <Categories title='Uno'>
+      {videos.mylist.length > 0 && (
+        <Categories title='My list'>
+          <Carousel>
+            <CarouselItem />
+          </Carousel>
+        </Categories>
+      )}
+      <Categories title='Trends'>
         <Carousel>
-          <CarouselItem />
-          <CarouselItem />
+          {videos.trends.map((item) => (
+            <CarouselItem key={item.id} {...item} />
+          ))}
         </Carousel>
       </Categories>
       <Footer />
