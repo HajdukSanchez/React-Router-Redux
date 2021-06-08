762961c7dbe24e39b14719076887a26705aabfc6
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-06-08 12:21:08 -0500

    :construction: Container and styles

--- src/containers/Register.jsx
+++ src/containers/Register.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import "../assets/styles/containers/Register.scss";
 
 const Register = () => (
-  
-)
+  <section className='register'>
+    <section className='register__container'>
+      <h2>Register</h2>
+      <form className='register__container--form'>
+        <input className='input' type='text' placeholder='Name' />
+        <input className='input' type='text' placeholder='Mail' />
+        <input className='input' type='password' placeholder='Password' />
+        <button className='button'>Sign in</button>
+      </form>
+      <a href=''>Login</a>
+    </section>
+  </section>
+);
 
-export default Register
+export default Register;
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,15 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "../containers/Home";
 import Login from "../containers/Login";
+import Register from "../containers/Register";
 
 const App = () => (
   // This is the parent component that allow us to manage our routes
   <Router>
-    {/* This are the routes */}
-    <Route exact path='/' component={Home} />
-    <Route exact path='/login' component={Login} />
+    {/* With the SWITCH, REACT render the first coincidence */}
+    <Switch>
+      {/* This are the routes */}
+      <Route exact path='/' component={Home} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/register' component={Register} />
+    </Switch>
   </Router>
 );
 
