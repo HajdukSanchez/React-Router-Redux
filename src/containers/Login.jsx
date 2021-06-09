f3dd67c174ddfcc057f7d3acdfe322b913f4160b
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-06-09 12:08:05 -0500

    :construction: Login add

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,40 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../assets/styles/containers/Login.scss";
 
 import googleIcon from "../assets/static/google-icon.png";
 import twitterIcon from "../assets/static/twitter-icon.png";
 
-const Login = () => (
-  <section className='login'>
-    <section className='login__container'>
-      <h2>Log In</h2>
-      <form className='login__container--form'>
-        <input className='input' type='text' placeholder='Mail' />
-        <input className='input' type='password' placeholder='Password' />
-        <button className='button'>Login</button>
-        <div className='login__container--remember-me'>
-          <label>
-            <input type='checkbox' id='cbox1' value='first_checkbox' />
-            Remember me
-          </label>
-          <a href='/'>Forget my password</a>
-        </div>
-      </form>
-      <section className='login__container--social-media'>
-        <div>
-          <img src={googleIcon} /> Login with Google
-        </div>
-        <div>
-          <img src={twitterIcon} /> Login with Twitter
-        </div>
+const Login = () => {
+  const [form, setForm] = useState({ email: "" });
+  const handleInput = (event) => {
+    setForm({
+      ...form,
+      // Save the value depending the name of our tag and the value that contains
+      [event.target.name]: event.target.value,
+    });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(form);
+  };
+  return (
+    <section className='login'>
+      <section className='login__container'>
+        <h2>Log In</h2>
+        <form className='login__container--form' onSubmit={handleSubmit}>
+          <input className='input' type='text' placeholder='Mail' name='email' onChange={handleInput} />
+          <input className='input' type='password' placeholder='Password' name='password' onChange={handleInput} />
+          <button className='button'>Login</button>
+          <div className='login__container--remember-me'>
+            <label>
+              <input type='checkbox' id='cbox1' value='first_checkbox' />
+              Remember me
+            </label>
+            <a href='/'>Forget my password</a>
+          </div>
+        </form>
+        <section className='login__container--social-media'>
+          <div>
+            <img src={googleIcon} /> Login with Google
+          </div>
+          <div>
+            <img src={twitterIcon} /> Login with Twitter
+          </div>
+        </section>
+        <p className='login__container--register'>
+          You don't have an account
+          <Link to='/register'>Register</Link>
+        </p>
       </section>
-      <p className='login__container--register'>
-        You don't have an account
-        <Link to='/register'>Register</Link>
-      </p>
     </section>
-  </section>
-);
+  );
+};
 
 export default Login;
