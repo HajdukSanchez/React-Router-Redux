1d4bdaba4e979f161484cdcc77d3da1a7fc38cbe
Jozek Hajduk <76627513+HajdukSanchez@users.noreply.github.com>
2021-06-09 12:47:50 -0500

    :construction: Added gravatar image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,44 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import "../assets/styles/components/Header.scss";
+
+import gravatar from "../utils/gravatar";
 
 import logo from "../assets/static/logo-platzi-video-BW2.png";
 import userIcon from "../assets/static/user-icon.png";
 
-const Header = () => (
-  <header className='header'>
-    <Link to='/'>
-      <img className='header__img' src={logo} alt='Platzi Video' />
-    </Link>
-    <div className='header__menu'>
-      <div className='header__menu--profile'>
-        <img src={userIcon} alt='User icon' />
-        <p>Profile</p>
+const Header = (props) => {
+  const { user } = props;
+  // We use OBJECT for validate a value inside and object
+  const hasUser = Object.keys(user).length > 0; // This is validation for know if user has elements
+  return (
+    <header className='header'>
+      <Link to='/'>
+        <img className='header__img' src={logo} alt='Platzi Video' />
+      </Link>
+      <div className='header__menu'>
+        <div className='header__menu--profile'>
+          <img src={hasUser ? gravatar(user.email) : userIcon} alt={hasUser ? user.email : "User icon"} />
+          <p>Profile</p>
+        </div>
+        <ul>
+          <li>
+            <Link to='/login'>Log in</Link>
+          </li>
+          <li>
+            <Link to='/register'>Sign in</Link>
+          </li>
+        </ul>
       </div>
-      <ul>
-        <li>
-          <Link to='/login'>Log in</Link>
-        </li>
-        <li>
-          <Link to='/register'>Sign in</Link>
-        </li>
-      </ul>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  };
+};
+
+export default connect(mapStateToProps, null)(Header);
